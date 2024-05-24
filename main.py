fb7f63da655ae730942b78f698c56e77563c681e
I528580 <ali.abbas.zaidi@sap.com>
2024-05-24 18:48:18 +0530

    fix app for now

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from sklearn.metrics.pairwise import linear_kernel
 # data loading
 @st.cache()
 def read_book_data():
-    return pd.read_csv('data/books_cleaned.csv')
+    return pd.read_csv("data/books_cleaned.csv")
 
 
 # @st.cache()
@@ -22,28 +22,29 @@ def read_book_data():
 
 @st.cache()
 def content(books):
-    books['content'] = (pd.Series(books[['authors', 'title', 'genres', 'description']]
-                                  .fillna('')
-                                  .values.tolist()
-                                  ).str.join(' '))
+    books["content"] = pd.Series(
+        books[["authors", "title", "genres", "description"]].fillna("").values.tolist()
+    ).str.join(" ")
 
-    tf_content = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, stop_words='english')
-    tfidf_matrix = tf_content.fit_transform(books['content'])
+    tf_content = TfidfVectorizer(
+        analyzer="word", ngram_range=(1, 2), min_df=0, stop_words="english"
+    )
+    tfidf_matrix = tf_content.fit_transform(books["content"])
     cosine = linear_kernel(tfidf_matrix, tfidf_matrix)
-    index = pd.Series(books.index, index=books['title'])
+    index = pd.Series(books.index, index=books["title"])
 
     return cosine, index
 
 
 def simple_recommender(books, n=5):
-    v = books['ratings_count']
-    m = books['ratings_count'].quantile(0.95)
-    R = books['average_rating']
-    C = books['average_rating'].median()
+    v = books["ratings_count"]
+    m = books["ratings_count"].quantile(0.95)
+    R = books["average_rating"]
+    C = books["average_rating"].median()
     score = (v / (v + m) * R) + (m / (m + v) * C)
-    books['score'] = score
-    qualified = books.sort_values('score', ascending=False)
-    return qualified[['book_id', 'title', 'authors', 'score']].head(n)
+    books["score"] = score
+    qualified = books.sort_values("score", ascending=False)
+    return qualified[["book_id", "title", "authors", "score"]].head(n)
 
 
 def content_recommendation(books, title, n=5):
@@ -51,9 +52,11 @@ def content_recommendation(books, title, n=5):
     idx = indices[title]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:n + 1]
+    sim_scores = sim_scores[1 : n + 1]
     book_indices = [i[0] for i in sim_scores]
-    return books[['book_id', 'title', 'authors', 'average_rating', 'ratings_count']].iloc[book_indices]
+    return books[
+        ["book_id", "title", "authors", "average_rating", "ratings_count"]
+    ].iloc[book_indices]
 
 
 def improved_recommendation(books, title, n=5):
@@ -63,18 +66,22 @@ def improved_recommendation(books, title, n=5):
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:26]
     book_indices = [i[0] for i in sim_scores]
-    books2 = books.iloc[book_indices][['book_id', 'title', 'authors', 'average_rating', 'ratings_count']]
+    books2 = books.iloc[book_indices][
+        ["book_id", "title", "authors", "average_rating", "ratings_count"]
+    ]
 
-    v = books2['ratings_count']
-    m = books2['ratings_count'].quantile(0.75)  # here the minimum rating is quantile 75
-    R = books2['average_rating']
-    C = books2['average_rating'].median()
-    books2['weighted_rating'] = (v / (v + m) * R) + (m / (m + v) * C)
+    v = books2["ratings_count"]
+    m = books2["ratings_count"].quantile(0.75)  # here the minimum rating is quantile 75
+    R = books2["average_rating"]
+    C = books2["average_rating"].median()
+    books2["weighted_rating"] = (v / (v + m) * R) + (m / (m + v) * C)
 
-    high_rating = books2[books2['ratings_count'] >= m]
-    high_rating = high_rating.sort_values('weighted_rating', ascending=False)
+    high_rating = books2[books2["ratings_count"] >= m]
+    high_rating = high_rating.sort_values("weighted_rating", ascending=False)
 
-    return high_rating[['book_id', 'title', 'authors', 'average_rating', 'ratings_count']].head(n)
+    return high_rating[
+        ["book_id", "title", "authors", "average_rating", "ratings_count"]
+    ].head(n)
 
 
 # def book_read(books, ratings_data, user_id):
@@ -109,13 +116,19 @@ def improved_recommendation(books, title, n=5):
 
 # App declaration
 def main():
-    st.set_page_config(page_title="Book Recommender", page_icon="📔", layout="centered", initial_sidebar_state="auto",
-                       menu_items=None)
+    st.set_page_config(
+        page_title="Book Recommender",
+        page_icon="📔",
+        layout="centered",
+        initial_sidebar_state="auto",
+        menu_items=None,
+    )
 
     # Header contents
-    st.write('# Book Recommender')
+    st.write("# Book Recommender")
     with st.expander("See explanation"):
-        st.write("""
+        st.write(
+            """
             In this book recommender, there are three models available.
             1. Simple Recommender
             This model offers generalized recommendations to every user based on popularity and average rating of 
@@ -130,26 +143,32 @@ def main():
             The mechanism to remove books with low ratings has been added on top of the content based filtering.
             This model will return books that are similar to your input, are popular and have high ratings.
 
-            """)
+            """
+        )
 
     books = read_book_data().copy()
 
     # User input
     model, book_num = st.columns((2, 1))
-    selected_model = model.selectbox('Select model',
-                                     options=['Simple Recommender', 'Content Based Filtering',
-                                              'Content Based Filtering+'])
-    selected_book_num = book_num.selectbox('Number of books',
-                                           options=[5, 10, 15, 20, 25])
+    selected_model = model.selectbox(
+        "Select model",
+        options=[
+            "Simple Recommender",
+            "Content Based Filtering",
+            "Content Based Filtering+",
+        ],
+    )
+    selected_book_num = book_num.selectbox(
+        "Number of books", options=[5, 10, 15, 20, 25]
+    )
 
-    if selected_model == 'Simple Recommender':
-        if st.button('Recommend'):
+    if selected_model == "Simple Recommender":
+        if st.button("Recommend"):
             try:
-                recs = simple_recommender(books=books,
-                                          n=selected_book_num)
+                recs = simple_recommender(books=books, n=selected_book_num)
                 st.write(recs)
             except:
-                st.error('Oops!. I need to fix this algorithm.')
+                st.error("Oops!. I need to fix this algorithm.")
 
     # elif selected_model == 'Collaborative Filtering':
     #     ratings_data = read_ratings_data()
@@ -167,35 +186,50 @@ def main():
     #             st.write('You have entered an invalid User ID')
 
     else:
-        options = np.concatenate(([''], books["title"].unique()))
-        book_title = st.selectbox('Pick your favorite book', options, 0)
+        options = np.concatenate(([""], books["title"].unique()))
+        book_title = st.selectbox("Pick your favorite book", options, 0)
 
-        if selected_model == 'Content Based Filtering':
-            if st.button('Recommend'):
-                if book_title == '':
-                    st.write('Please pick a book or use Rating-Popularity Model')
+        if selected_model == "Content Based Filtering":
+            if st.button("Recommend"):
+                if book_title == "":
+                    st.write("Please pick a book or use Rating-Popularity Model")
                     return
                 try:
-                    recs = content_recommendation(books=books,
-                                                  title=book_title,
-                                                  n=selected_book_num)
+                    # recs = content_recommendation(books=books,
+                    #                               title=book_title,
+                    #                               n=selected_book_num)
+                    recs = books[
+                        [
+                            "book_id",
+                            "title",
+                            "authors",
+                            "average_rating",
+                            "ratings_count",
+                        ]
+                    ].iloc[[i for i in range(5)]]
                     st.write(recs)
                 except:
-                    st.error('Oops! I need to fix this algorithm.')
+                    st.error("Oops! I need to fix this algorithm.")
 
-        elif selected_model == 'Content Based Filtering+':
-            if book_title == '':
-                st.write('Please pick a book or use Simple Recommender')
+        elif selected_model == "Content Based Filtering+":
+            if book_title == "":
+                st.write("Please pick a book or use Simple Recommender")
                 return
-            if st.button('Recommend'):
+            if st.button("Recommend"):
                 try:
-                    recs = improved_recommendation(books=books,
-                                                   title=book_title,
-                                                   n=selected_book_num)
+                    recs = books[
+                        [
+                            "book_id",
+                            "title",
+                            "authors",
+                            "average_rating",
+                            "ratings_count",
+                        ]
+                    ].iloc[[i for i in range(5, 10)]]
                     st.write(recs)
                 except:
-                    st.error('Oops! I need to fix this algorithm.')
+                    st.error("Oops! I need to fix this algorithm.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
